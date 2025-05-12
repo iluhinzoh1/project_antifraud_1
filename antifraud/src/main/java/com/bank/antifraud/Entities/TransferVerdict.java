47277a264a54004555c8a465e5c351ef31e586f1
iluhinzoh1 <165574083+iluhinzoh1@users.noreply.github.com>
2025-05-12 14:43:50 +0300

    Update TransferVerdict.java

--- antifraud/src/main/java/com/bank/antifraud/Entities/TransferVerdict.java
+++ antifraud/src/main/java/com/bank/antifraud/Entities/TransferVerdict.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Класс для отправки подозрительных
+ * данных в другие микросервисы в понятном формате (id, заблокали/не заблокали, причина)
+ */
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
