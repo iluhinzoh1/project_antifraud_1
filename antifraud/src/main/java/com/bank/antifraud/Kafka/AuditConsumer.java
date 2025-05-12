9836065fd64b7f8f00827cc2baf42d56f9d36c12
iluhinzoh1 <165574083+iluhinzoh1@users.noreply.github.com>
2025-05-12 14:44:21 +0300

    Update AuditConsumer.java

--- antifraud/src/main/java/com/bank/antifraud/Kafka/AuditConsumer.java
+++ antifraud/src/main/java/com/bank/antifraud/Kafka/AuditConsumer.java
@@ -10,6 +10,11 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+/**
+ * Класс для получения подозрительных
+ * данных из AuditProducer и сохранения их в базе данных.
+ */
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
