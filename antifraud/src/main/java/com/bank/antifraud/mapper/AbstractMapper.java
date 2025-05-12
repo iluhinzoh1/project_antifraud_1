e5c0ce3178cb84970c22139df86d66b849e04774
iluhinzoh1 <165574083+iluhinzoh1@users.noreply.github.com>
2025-05-12 14:47:55 +0300

    Update AbstractMapper.java

--- antifraud/src/main/java/com/bank/antifraud/mapper/AbstractMapper.java
+++ antifraud/src/main/java/com/bank/antifraud/mapper/AbstractMapper.java
@@ -3,6 +3,9 @@ package com.bank.antifraud.mapper;
 import com.bank.antifraud.Entities.TransferChecked;
 import org.mapstruct.MappingTarget;
 
+/**
+ * Абстрактный маппер для преобразования сущности в dto и обратно.
+ */
 public interface AbstractMapper<D, E> {
     D toDto(E entity);
     E toEntity(D dto);
